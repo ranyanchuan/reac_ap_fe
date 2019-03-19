8f51d5dba08ce9a9d08fcd9489d33e4de6400156
LiuYueKai <liuyuekainihao@163.com>
2019-03-19 15:45:19 +0800

    提交测试页面,管理中心完成之后删除

---
+++ ucf-apps/appTest/src/app.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'core-js/es6/array';
+import 'core-js/es7/array';
+// import logger from "redux-logger";
+import mirror, { render,Router } from "mirrorx";
+import MainLayout from "layout";
+import Routes from './routes'
+import 'static/trd/tinper-bee/assets/tinper-bee.css'
+import { setCookie } from 'utils/index';
+setCookie('portalid',GROBAL_PORTAL_ID)
+
+
+require('es6-promise').polyfill();
+
+import Intl from 'components/Intl/index.js'
+
+const MiddlewareConfig = [];
+
+// if(__MODE__ == "development") MiddlewareConfig.push(logger);
+
+mirror.defaults({
+    historyMode: "hash",
+    middlewares: MiddlewareConfig
+});
+
+
+
+render(<Intl>
+    <Router>
+        <MainLayout Routes={Routes} />
+    </Router>
+</Intl>, document.querySelector("#login_div"));
