635b13423f060aa6d073cabe874f5c4ffb385f36
LiuYueKai <liuyuekainihao@163.com>
2019-04-08 15:49:49 +0800

    解决index.html引用路径不对的问题

--- build.js
+++ build.js
@@ -14,10 +14,10 @@ rm(path.resolve(__dirname, './build'), err => {
       cpr(path.resolve(__dirname, './ucf-publish/wbalone/index/index.js'), path.resolve(__dirname, './build/' + packageName + '/index/index.js'), {}, err => {
         cpr(path.resolve(__dirname, './ucf-publish/wbalone/login'), path.resolve(__dirname, './build/' + packageName + '/pages/login'), {}, err => {
           fs.rename('./build/' + packageName + '/pages/login/index.html', './build/' + packageName + '/pages/login/login.html', err => {
-            let htmlPath = path.resolve(__dirname + '/build/' + packageName + '/index.html');
-            let htmlStr = fs.readFileSync(htmlPath);
-            htmlStr = htmlStr.toString().replace(/..\/index/g,'./index')
-            fs.writeFileSync(htmlPath, htmlStr);
+            // let htmlPath = path.resolve(__dirname + '/build/' + packageName + '/index.html');
+            // let htmlStr = fs.readFileSync(htmlPath);
+            // htmlStr = htmlStr.toString().replace(/..\/index/g,'./index')
+            // fs.writeFileSync(htmlPath, htmlStr);
 
             var d = new Date();
             var dateStr = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
