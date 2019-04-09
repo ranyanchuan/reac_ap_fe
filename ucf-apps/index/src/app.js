0c33b87ecc6067c60df87972ee12333624ac91c4
LiuYueKai <liuyuekainihao@163.com>
2019-04-09 15:55:06 +0800

    ie

--- ucf-apps/index/src/app.js
+++ ucf-apps/index/src/app.js
@@ -6,22 +6,6 @@ import Routes from './routes'
 import 'static/trd/tinper-bee/assets/tinper-bee.css'
 import { setCookie } from 'utils/index';
 setCookie('portalid',GROBAL_PORTAL_ID)
-if (!Element.prototype.matches) {
-    Element.prototype.matches = Element.prototype.msMatchesSelector || 
-                            Element.prototype.webkitMatchesSelector;
-}
-
-if (!Element.prototype.closest) {
-Element.prototype.closest = function(s) {
-    var el = this;
-
-    do {
-    if (el.matches(s)) return el;
-    el = el.parentElement || el.parentNode;
-    } while (el !== null && el.nodeType === 1);
-    return null;
-};
-}
 
 import Intl from 'components/Intl/index.js'
 
--- ucf-common/src/layout/Header/Header.js
+++ ucf-common/src/layout/Header/Header.js
@@ -25,6 +25,22 @@ import {ConnectedHeaderRight,ConnectedHeaderCenter} from 'ucf-apps/index/src/con
 // const Toggle = Navbar.Toggle;
 const Nav = Navbar.Nav;
 
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector || 
+                            Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+Element.prototype.closest = function(s) {
+    var el = this;
+
+    do {
+    if (el.matches(s)) return el;
+    el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+};
+}
 
 class App extends Component {
     constructor(props, context) {
