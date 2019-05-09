1a7e16be0b6efe5c6ab71de52dbf38ea8c9b9d76
LiuYueKai <liuyuekainihao@163.com>
2019-05-09 14:10:35 +0800

    图标字体离线处理

--- build.js
+++ build.js
@@ -9,34 +9,36 @@ rm(path.resolve(__dirname, './build'), err => {
   fs.mkdirSync(path.resolve(__dirname, './build/' + packageName + '/pages'))
   fs.mkdirSync(path.resolve(__dirname, './build/' + packageName + '/index'))
   fs.mkdirSync(path.resolve(__dirname, './build/' + packageName + '/pages/login'))
+  fs.mkdirSync(path.resolve(__dirname, './build/' + packageName + '/index/static'))
   cpr(path.resolve(__dirname, './ucf-publish/wbalone/index/index.html'), path.resolve(__dirname, './build/' + packageName + '/index.html'), {}, err => {
     cpr(path.resolve(__dirname, './ucf-publish/wbalone/index/index.css'), path.resolve(__dirname, './build/' + packageName + '/index/index.css'), {}, err => {
       cpr(path.resolve(__dirname, './ucf-publish/wbalone/index/index.js'), path.resolve(__dirname, './build/' + packageName + '/index/index.js'), {}, err => {
         cpr(path.resolve(__dirname, './ucf-publish/wbalone/login'), path.resolve(__dirname, './build/' + packageName + '/pages/login'), {}, err => {
-          fs.rename('./build/' + packageName + '/pages/login/index.html', './build/' + packageName + '/pages/login/login.html', err => {
-            // let htmlPath = path.resolve(__dirname + '/build/' + packageName + '/pages/login/login.html');
-            // let htmlStr = fs.readFileSync(htmlPath);
-            // htmlStr = htmlStr.toString().replace(/..\/wbalone\//g,'')
-            // fs.writeFileSync(htmlPath, htmlStr);
+          cpr(path.resolve(__dirname, './ucf-common/src/static'), path.resolve(__dirname, './build/' + packageName + '/index/static'), {}, err => {
+            fs.rename('./build/' + packageName + '/pages/login/index.html', './build/' + packageName + '/pages/login/login.html', err => {
+              // let htmlPath = path.resolve(__dirname + '/build/' + packageName + '/pages/login/login.html');
+              // let htmlStr = fs.readFileSync(htmlPath);
+              // htmlStr = htmlStr.toString().replace(/..\/wbalone\//g,'')
+              // fs.writeFileSync(htmlPath, htmlStr);
 
-            let htmlPath = path.resolve(__dirname + '/build/' + packageName + '/index.html');
-            let htmlStr = fs.readFileSync(htmlPath);
-            htmlStr = htmlStr.toString().replace(/..\/index/g,'./index')
-            fs.writeFileSync(htmlPath, htmlStr);
+              let htmlPath = path.resolve(__dirname + '/build/' + packageName + '/index.html');
+              let htmlStr = fs.readFileSync(htmlPath);
+              htmlStr = htmlStr.toString().replace(/..\/index/g,'./index')
+              fs.writeFileSync(htmlPath, htmlStr);
 
-            var d = new Date();
-            var dateStr = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-            var addStr = '//  date:  ' + dateStr + '\r\n';
-            var addDateStr = function(fsPath){
-              var str1 = fs.readFileSync(path.resolve(__dirname, fsPath)).toString();
-              str1 = addStr + str1;
-              fs.writeFileSync(path.resolve(__dirname, fsPath), str1);
-            }
+              var d = new Date();
+              var dateStr = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+              var addStr = '//  date:  ' + dateStr + '\r\n';
+              var addDateStr = function(fsPath){
+                var str1 = fs.readFileSync(path.resolve(__dirname, fsPath)).toString();
+                str1 = addStr + str1;
+                fs.writeFileSync(path.resolve(__dirname, fsPath), str1);
+              }
 
-            addDateStr('./build/' + packageName + '/index/index.js')
-            addDateStr('./build/' + packageName + '/pages/login/index.js')
+              addDateStr('./build/' + packageName + '/index/index.js')
+              addDateStr('./build/' + packageName + '/pages/login/index.js')
+            })
           })
-
           
         })
       })
