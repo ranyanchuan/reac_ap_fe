4104f664fec80a5364f752dffa66918c72001c94
LiuYueKai <liuyuekainihao@163.com>
2019-05-22 10:44:15 +0800

    完善打包逻辑

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ const rm = require('rimraf')
 const cpr = require('cpr').cpr
 const path = require('path')
 const packageName = require('./ucf.config.js')().global_env.GROBAL_PACKAGE_NAME.replace(/"/g,'');
+const publishContext = require('./ucf.config.js')().context.replace(/"/g,'');
 rm(path.resolve(__dirname, './build'), err => {
   fs.mkdirSync(path.resolve(__dirname, './build'))
   fs.mkdirSync(path.resolve(__dirname, './build/' + packageName))
@@ -10,17 +11,17 @@ rm(path.resolve(__dirname, './build'), err => {
   fs.mkdirSync(path.resolve(__dirname, './build/' + packageName + '/index'))
   fs.mkdirSync(path.resolve(__dirname, './build/' + packageName + '/pages/login'))
   fs.mkdirSync(path.resolve(__dirname, './build/' + packageName + '/index/static'))
-  cpr(path.resolve(__dirname, './ucf-publish/iuap-lightportal-fe/index/index.html'), path.resolve(__dirname, './build/' + packageName + '/index.html'), {}, err => {
-    cpr(path.resolve(__dirname, './ucf-publish/iuap-lightportal-fe/index/index.css.gz'), path.resolve(__dirname, './build/' + packageName + '/index/index.css.gz'), {}, err => {
-      cpr(path.resolve(__dirname, './ucf-publish/iuap-lightportal-fe/index/index.css'), path.resolve(__dirname, './build/' + packageName + '/index/index.css'), {}, err => {
-        cpr(path.resolve(__dirname, './ucf-publish/iuap-lightportal-fe/index/index.js.gz'), path.resolve(__dirname, './build/' + packageName + '/index/index.js.gz'), {}, err => {
-          cpr(path.resolve(__dirname, './ucf-publish/iuap-lightportal-fe/index/index.js'), path.resolve(__dirname, './build/' + packageName + '/index/index.js'), {}, err => {
-            cpr(path.resolve(__dirname, './ucf-publish/iuap-lightportal-fe/login'), path.resolve(__dirname, './build/' + packageName + '/pages/login'), {}, err => {
+  cpr(path.resolve(__dirname, './ucf-publish/' + publishContext + '/index/index.html'), path.resolve(__dirname, './build/' + packageName + '/index.html'), {}, err => {
+    cpr(path.resolve(__dirname, './ucf-publish/' + publishContext + '/index/index.css.gz'), path.resolve(__dirname, './build/' + packageName + '/index/index.css.gz'), {}, err => {
+      cpr(path.resolve(__dirname, './ucf-publish/' + publishContext + '/index/index.css'), path.resolve(__dirname, './build/' + packageName + '/index/index.css'), {}, err => {
+        cpr(path.resolve(__dirname, './ucf-publish/' + publishContext + '/index/index.js.gz'), path.resolve(__dirname, './build/' + packageName + '/index/index.js.gz'), {}, err => {
+          cpr(path.resolve(__dirname, './ucf-publish/' + publishContext + '/index/index.js'), path.resolve(__dirname, './build/' + packageName + '/index/index.js'), {}, err => {
+            cpr(path.resolve(__dirname, './ucf-publish/' + publishContext + '/login'), path.resolve(__dirname, './build/' + packageName + '/pages/login'), {}, err => {
               cpr(path.resolve(__dirname, './ucf-common/src/static'), path.resolve(__dirname, './build/' + packageName + '/index/static'), {}, err => {
                 fs.rename('./build/' + packageName + '/pages/login/index.html', './build/' + packageName + '/pages/login/login.html', err => {
                   // let htmlPath = path.resolve(__dirname + '/build/' + packageName + '/pages/login/login.html');
                   // let htmlStr = fs.readFileSync(htmlPath);
-                  // htmlStr = htmlStr.toString().replace(/..\/iuap-lightportal-fe\//g,'')
+                  // htmlStr = htmlStr.toString().replace(/..\/' + packageName + '\//g,'')
                   // fs.writeFileSync(htmlPath, htmlStr);
 
                   let htmlPath = path.resolve(__dirname + '/build/' + packageName + '/index.html');
